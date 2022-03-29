6d36c5cdf775cedc747ce5e9c36b1788cea400dd
iremkvkpnr <kavakpinarirem1@gmail.com>
2022-03-29 10:12:47 +0300

    sınıfı selamladım.

--- 152120201069_q2/152120201069_q2.cpp
+++ 152120201069_q2/152120201069_q2.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "hello world!";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
